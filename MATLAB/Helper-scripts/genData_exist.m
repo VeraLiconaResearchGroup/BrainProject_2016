b13acb0b758df67b7f46aee506554bbeddb06b77
tjann <jann.tiffany@gmail.com>
2016-07-27 20:39:09 -0400

    matlab weird

--- MATLAB/Helper-scripts/genData_exist.m
+++ MATLAB/Helper-scripts/genData_exist.m
@@ -5,12 +5,16 @@
 %% Example execution: genData ADHD_Sub001.txt ADHD_Sub001
 % Note: genData is called by multisub so generally no need to call genData independently
 function genData_exist(filename, foldername)
-    existingfile=load(['datafolderinsilico/', filename]);
+      existingfile=load(['slidewin/', filename]);
+%     existingfile=load(['datafolderinsilico/', filename]);
 %     header  = textscan(fileID, '%s', 49);
 %     LFP = textscan(fileID, ['%f', repmat('%f',[1,48])], 204);
 %     LFP = cell2mat(LFP);
 %     LFP = LFP';
 %     Connectivity = ones(49,49);
 %     Params.fs = 0.500;
-    save([foldername, '/data/', foldername,'fmriCS100S1N204.mat'], 'existingfile');
+    LFP = existingfile.LFP;
+    Connectivity = existingfile.Connectivity;
+    Params.fs = 0.500;
+    save([foldername, '/data/', foldername,'fmriCS100S1N204.mat'], 'Connectivity', 'LFP', 'Params');
     mln_CalEvaN_exist([foldername],[foldername], 'GenerateData/structureN49scaleFree1', 'nmmParams', '49', '1', '204', '1', 'fMRI');
